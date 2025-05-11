c73a5974e88cd454bee90fb47b174288ba4fc2bb
Curry5004 <yunjin5004@naver.com>
2025-05-11 17:10:35 +0900

    project init

---
+++ src/main/java/com/wanted/WantedCqrsApplication.java
@@ -0,0 +1,11 @@
+package com.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedCqrsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WantedCqrsApplication.class, args);
+    }
+}
